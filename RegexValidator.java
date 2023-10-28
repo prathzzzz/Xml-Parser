4a989389acdbf28893f5133b411dbf24a2347038
prathzzzz <kariapratham@gmail.com>
2023-10-29 01:37:13 +0530

    Updated Regex

--- RegexValidator.java
+++ RegexValidator.java
@@ -3,6 +3,8 @@ import java.util.regex.Pattern;
 public class RegexValidator {
     public static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@(.+)$";
     public static final String DOB_PATTERN = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(19|20)\\d{2}$";
+    public static final String PHONE_NUMBER_PATTERN = "^[0-9]{10}$";
+    public static final String NAME_PATTERN = "^[A-Za-z\\s]+$";
 
     public static boolean isEmailValid(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
@@ -15,14 +17,12 @@ public class RegexValidator {
     }
 
     public static boolean isPhoneNumberValid(String phoneNumber) {
-        // Regex for a valid phone number (10 digits)
-        String regex = "^[0-9]{10}$";
-        return phoneNumber.matches(regex);
+        Pattern pattern = Pattern.compile(PHONE_NUMBER_PATTERN);
+        return pattern.matcher(phoneNumber).matches();
     }
 
     public static boolean isNameValid(String name) {
-        // Regex for a full name (alphabets with spaces)
-        String regex = "^[A-Za-z\\s]+$";
-        return name.matches(regex);
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(name).matches();
     }
 }
